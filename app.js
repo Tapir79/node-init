cfa93a9c7ad4c648b449d28ff610fe4e8cfe03b0
Tapir79 <saara.m.pakarinen@gmail.com>
2021-01-18 18:57:10 +0200

    add hello

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 
 const app = express()
 
+
+app.get('/', function (req, res) {
+    res.send('hello');
+  });
+
 app.listen(3000, () => {
   console.log('App listening on port 3000')
 })
+
