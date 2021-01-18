8eb6a1d8ad20385fc7f1c28aca1cc6513ddae73c
Tapir79 <saara.m.pakarinen@gmail.com>
2021-01-18 19:04:30 +0200

    azure port

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express')
 
 const app = express()
 
-
 app.get('/', function (req, res) {
     res.send('hello');
   });
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000')
-})
+const port = process.env.PORT || 3000;
+app.listen(port);
 
